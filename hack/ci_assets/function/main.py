38e550e410238e8c0971d29c215e024032fd002b
Liran BG <liranbg@gmail.com>
2021-04-05 11:38:02 -0400

    update function

--- hack/ci_assets/function/main.py
+++ hack/ci_assets/function/main.py
@@ -14,12 +14,37 @@
 
 import pkg_resources
 import http
+import json
 
 import nuclio_sdk
 
 
 def handler(context: nuclio_sdk.Context, event: nuclio_sdk.Event):
-    context.logger.debug_with("Received request", event=event.to_json())
+    context.logger.debug_with(
+        "Received request",
+        event=json.dumps(
+            {
+                "id": event.id,
+                "eventType": event.trigger.kind,
+                "contentType": event.content_type,
+                "headers": {
+                    ensure_str(header): ensure_str(value)
+                    for header, value in event.headers.items()
+                },
+                "timestamp": event.timestamp.isoformat("T") + "Z",
+                "path": event.path,
+                "url": event.url,
+                "method": event.method,
+                "type": event.type,
+                "typeVersion": event.type_version,
+                "version": event.version,
+                "body": event.body
+                if isinstance(event.body, dict)
+                else event.body.decode("utf8"),
+            },
+            default=json_default,
+        ),
+    )
     return context.Response(
         headers=event.headers,
         body={
@@ -32,3 +57,19 @@ def handler(context: nuclio_sdk.Context, event: nuclio_sdk.Event):
 
 def get_sdk_version():
     return pkg_resources.get_distribution("nuclio_sdk").version
+
+
+def json_default(s):
+    if type(s) is bytes:
+        return ensure_str(s)
+    return s
+
+
+def ensure_str(s, encoding="utf-8", errors="strict"):
+
+    # Optimization: Fast return for the common case.
+    if type(s) is str:
+        return s
+    if isinstance(s, bytes):
+        return s.decode(encoding, errors)
+    raise TypeError(f"not expecting type '{type(s)}'")
