61439363ebf2957a55f915b913c53d5b4fe1019e
rokatyy <ekaterina_molchanova@mckinsey.com>
2025-12-15 11:08:32 +0000

    delete older pythons to setup

--- setup.py
+++ setup.py
@@ -73,8 +73,6 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
